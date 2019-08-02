6accc99f3b9006b81582bfd6ffd1a66031f60460
zenVentzi <v.marinov96@gmail.com>
2019-08-02 09:17:29 +0300

    Add Components folder

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import TestApp from './Components/TestApp';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => TestApp);
