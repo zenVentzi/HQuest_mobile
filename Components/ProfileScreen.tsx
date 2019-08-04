f4208185f216a6d1c30f1213e810f881c54c1c17
zenVentzi <v.marinov96@gmail.com>
2019-08-04 15:45:06 +0300

    Add template profile pic to profile screen

--- Components/ProfileScreen.tsx
+++ Components/ProfileScreen.tsx
@@ -1,12 +1,27 @@
 import React, { Fragment, Component } from 'react';
-import { Button, Text } from 'react-native';
+import { Button, Text, StyleSheet, View, Image } from 'react-native';
 
 export default class ProfileScreen extends Component<any, any> {
-  static navigationOptions = {
-    title: 'Welcome',
-  };
   render() {
-    const { navigate } = this.props.navigation;
-    return <Text>Profile screen</Text>;
+    return (
+      <View style={styles.container}>
+        <Image style={styles.profilePic} source={require('./profile.png')} />
+      </View>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    backgroundColor: 'black',
+  },
+  profilePic: {
+    marginTop: 30,
+    width: 200,
+    height: 200,
+    borderRadius: 100,
+  },
+});
