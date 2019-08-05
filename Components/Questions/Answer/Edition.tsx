e820a3fbee4aced125f13a5d68a0e284db38c2f1
zenVentzi <v.marinov96@gmail.com>
2019-08-05 22:16:58 +0300

    Add Likes and Comments buttons

--- Components/Questions/Answer/Edition.tsx
+++ Components/Questions/Answer/Edition.tsx
@@ -1,5 +1,8 @@
 import React, { Component, Fragment } from 'react';
 import { Text, StyleSheet, View } from 'react-native';
+import { Icon } from 'native-base';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import TextButton from '../../Reusable/TextButton';
 
 export default class Edition extends Component {
   render() {
@@ -9,9 +12,31 @@ export default class Edition extends Component {
           - Well, things are the way they are. It is what it is. Fill this damn
           space
         </Text>
+        <View style={styles.row}>
+          <TouchableOpacity>
+            <Icon type="AntDesign" name="like1" />
+          </TouchableOpacity>
+          <TextButton
+            title={`0 likes`}
+            onPress={() => {}}
+            style={styles.likesTextBtn}
+          />
+          <TextButton
+            title={`0 comments`}
+            onPress={() => {}}
+            style={styles.commentsTextBtn}
+          />
+        </View>
       </Fragment>
     );
   }
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    width: '100%',
+  },
+  likesTextBtn: { height: 23, marginTop: 4 },
+  commentsTextBtn: { height: 23, marginTop: 4 },
+});
