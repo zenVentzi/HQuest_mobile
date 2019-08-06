b33b2399ed3f0860192f6b4e59a8ee646ce8321a
zenVentzi <v.marinov96@gmail.com>
2019-08-06 09:50:07 +0300

    Add navbar buttons

--- Components/App.tsx
+++ Components/App.tsx
@@ -8,25 +8,77 @@ import HomeScreen from './HomeScreen';
 import ProfileScreen from './ProfileScreen';
 import SettingsScreen from './SettingsScreen';
 import React, { Component, Fragment } from 'react';
-import { View } from 'react-native';
+import { View, StyleSheet, TouchableOpacity } from 'react-native';
 import { Button, Icon } from 'native-base';
 
 const StackNavConfig: StackNavigatorConfig = {
   defaultNavigationOptions: {
     header: ({ navigation }) => (
-      <View
-        style={{ backgroundColor: 'black', height: 50, flexDirection: 'row' }}>
-        <Button
-          transparent
-          onPress={() => {
-            navigation.openDrawer();
-          }}>
-          <Icon name="menu" style={{ color: 'white', fontSize: 40 }} />
-        </Button>
+      <View style={styles.container}>
+        <View style={styles.headerLeft}>
+          <TouchableOpacity
+            style={styles.drawerBtn}
+            onPress={() => {
+              navigation.openDrawer();
+            }}>
+            <Icon name="menu" style={{ color: 'white', fontSize: 45 }} />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.headerRight}>
+          <TouchableOpacity
+            style={styles.newsfeedBtn}
+            onPress={() => {
+              // navigation.openDrawer();
+            }}>
+            <Icon
+              type="FontAwesome"
+              name="newspaper-o"
+              style={{ color: 'white', fontSize: 35 }}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.newsfeedBtn}
+            onPress={() => {
+              // navigation.openDrawer();
+            }}>
+            <Icon
+              type="FontAwesome"
+              name="user-circle"
+              style={{ color: 'white', fontSize: 35 }}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.newsfeedBtn}
+            onPress={() => {
+              // navigation.openDrawer();
+            }}>
+            <Icon
+              type="FontAwesome"
+              name="bell-o"
+              style={{ color: 'white', fontSize: 35 }}
+            />
+          </TouchableOpacity>
+        </View>
       </View>
     ),
   },
 };
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'black',
+    height: 50,
+    maxHeight: 50,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 10,
+    paddingTop: 10,
+  },
+  headerLeft: {},
+  headerRight: { flexDirection: 'row' },
+  drawerBtn: { marginTop: -8, marginLeft: 5 },
+  newsfeedBtn: { marginRight: 13 },
+});
 
 export const HomeStackNavigator = createStackNavigator(
   {
